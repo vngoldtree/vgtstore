75981acfb4e667f298afb210a176d934dbc1d6f2
Sang.Bui <buithanhsang2010@gmail.com>
2016-05-16 12:44:58 +0700

    Extend medium size for image

--- spree_vgt_frontend/app/models/spree/image_decorator.rb
+++ spree_vgt_frontend/app/models/spree/image_decorator.rb
@@ -1,20 +1,5 @@
 module Spree
-	Image.class_eval do
-		attachment_definitions[:attachment][:styles].merge!(
-	    	:home_product => '500x500>'
-		)
-
-		attachment_definitions[:attachment][:styles].each do |style, v|
-		    define_method "#{style}_image" do |product, *options|
-		      options = options.first || {}
-		      if product.images.empty?
-		        image_tag "noimage/#{style}.jpg", options
-		      else
-		        image = product.images.first
-		        options.reverse_merge! :alt => image.alt.blank? ? product.name : image.alt
-		        image_tag image.attachment.url(style), options
-		      end
-		    end
-	  	end
-	end
+	Image.attachment_definitions[:attachment][:styles].merge!(
+      :medium => '500x500>'
+     )
 end
