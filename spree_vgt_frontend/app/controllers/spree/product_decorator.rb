9b785e2db54385a148f7c76b3ff79139aeb7c419
Sang.Bui <buithanhsang2010@gmail.com>
2016-05-21 21:05:40 +0700

    Add list categories on product page

--- spree_vgt_frontend/app/controllers/spree/product_decorator.rb
+++ spree_vgt_frontend/app/controllers/spree/product_decorator.rb
@@ -2,6 +2,15 @@ module Spree
 	ProductsController.class_eval do
 		alias_method :orig_show, :show
 		def show
+			@parent_taxons = []
+			if @product.taxons
+				@parent_taxons << @product.taxons.first
+				tmp = @product.taxons.first
+				while tmp.parent_id
+					tmp =  Taxon.friendly.find(tmp.parent_id)
+					@parent_taxons << tmp
+				end
+			end
 			@taxonomies = Spree::Taxonomy.includes(root: :children)
 			return orig_show
 		end
