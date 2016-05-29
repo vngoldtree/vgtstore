8cdafbfefd4897551da8fb507ea37f631a7a7e82
Sang.Bui <buithanhsang2010@gmail.com>
2016-05-29 16:24:11 +0700

    Update order line

---
+++ spree_vgt_frontend/app/assets/javascripts/spree/frontend/cart.js
@@ -0,0 +1,9 @@
+$(document).ready(function($) {
+    if (($('form#update-cart')).is('*')) {
+      ($('form#update-cart a.delete')).show().one('click', function() {
+        ($(this)).parents('.line-item').first().find('input.line_item_quantity').val(0);
+        ($(this)).parents('form').first().submit();
+        return false;
+      });
+    }
+  });
