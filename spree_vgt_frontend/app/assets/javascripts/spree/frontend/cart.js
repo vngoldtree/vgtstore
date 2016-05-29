e7c7f8e393ff209c3a006bb2f7080d8d57b07f8a
Sang.Bui <buithanhsang2010@gmail.com>
2016-05-29 16:27:39 +0700

    return true when finish remove item

--- spree_vgt_frontend/app/assets/javascripts/spree/frontend/cart.js
+++ spree_vgt_frontend/app/assets/javascripts/spree/frontend/cart.js
@@ -3,7 +3,7 @@ $(document).ready(function($) {
       ($('form#update-cart a.delete')).show().one('click', function() {
         ($(this)).parents('.line-item').first().find('input.line_item_quantity').val(0);
         ($(this)).parents('form').first().submit();
-        return false;
+        return true;
       });
     }
   });
